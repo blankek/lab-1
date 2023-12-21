e90ac9a725b52dd1eb3412b71887be0580e3d57f
blankec <Komba-rus05@mail.ru>
2023-12-21 20:13:17 +0500

    lab2

--- Laba1/Program.cs
+++ Laba1/Program.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Schema;
 
+
 namespace lab1
 {
     internal class Program
@@ -21,16 +22,78 @@ namespace lab1
             
         }
 
-        public static List<object> OperationsAndNumbers(string str)
-        {            
-            List<object> numbers = new List<object>();            
+        public static ArrayList OperationsAndNumbers(string str)
+        {
+            List<char> operations = new List<char>();
+            List<double> numbers = new List<double>();            
             char[] testOperation = { '-', '*', '/', '+' };
             string[] arrayNumber = str.Split(testOperation, StringSplitOptions.RemoveEmptyEntries);
             foreach (string num in arrayNumber)
             {
-                numbers.Add(num);
-            }                       
-            return numbers;
-        }                   
+                numbers.Add(double.Parse(num));
+            }
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (testOperation.Contains(str[i]))
+                {
+                    operations.Add(str[i]);
+                }
+            }
+            Console.WriteLine($"список чисел: {string.Join(" ", numbers)}");
+            Console.WriteLine($"список операций: {string.Join(" ", operations)}");
+            ArrayList lists = new ArrayList() { operations, numbers };
+            return lists;
+        }
+        public static void GetAnsewer(ArrayList lists)
+        {
+            List<char> operations = (List<char>)lists[0];
+            List<double> numbers = (List<double>)lists[1];
+            
+            while (numbers.Count != 1)
+            {
+                for (int i =0; i < operations.Count; i++)
+                {
+                    double number;
+                    if (operations[i] == '*' || operations[i] == '/')
+                    {
+                        if (operations[i] == '*')
+                        {
+                            number = numbers[i] * numbers[i + 1];
+                        }
+
+                        else
+                        {
+                            number = numbers[i] / numbers[i + 1];
+                        }
+                        numbers.RemoveAt(i+1);
+                        numbers.RemoveAt(i);
+                        operations.RemoveAt(i);
+                        numbers.Insert(i, number);
+                    }
+                    else if (!(operations.Contains('*')|| operations.Contains('/')))
+                    {
+                        if (operations[i] == '+')
+                        {
+                            number = numbers[i] + numbers[i + 1];
+                        }
+                        else
+                        {
+                            number = numbers[i] - numbers[i + 1];
+                        }
+                        numbers.RemoveAt(i + 1);
+                        numbers.RemoveAt(i);
+                        numbers.Insert(i, number);
+                        operations.RemoveAt(i);
+                        
+                    }
+                }
+                
+               
+            }
+            Console.WriteLine($"ответ: {string.Join(" ", numbers)}");
+
+        }
+        
     }
 }
