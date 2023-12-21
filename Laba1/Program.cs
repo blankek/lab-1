3e3c45de899949df66ee1d1ba2364488ab9b7d0c
blankec <Komba-rus05@mail.ru>
2023-12-21 20:08:06 +0500

    lab2

--- Laba1/Program.cs
+++ Laba1/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Schema;
@@ -13,34 +16,21 @@ namespace lab1
         {
             Console.WriteLine("Введите выражение:");
             string str = Console.ReadLine();
-            Number(str);
-            Operation(str);
+            str = str.Replace(" ", string.Empty);
+            GetAnsewer(OperationsAndNumbers(str));
+            
         }
-        public static void Number(string str)
-        {
-            List<int> numbers = new List<int>();
-            string[] arrayNumber = str.Split('-', '*', '/', '+');
 
+        public static List<object> OperationsAndNumbers(string str)
+        {            
+            List<object> numbers = new List<object>();            
+            char[] testOperation = { '-', '*', '/', '+' };
+            string[] arrayNumber = str.Split(testOperation, StringSplitOptions.RemoveEmptyEntries);
             foreach (string num in arrayNumber)
             {
-                numbers.Add(int.Parse(num));
-            }
-
-            Console.WriteLine(string.Join(" ", numbers));
-        }
-
-        public static void Operation(string str)
-        {
-            List<char> chars = new List<char>();
-            char[] testOperation = { '-', '*', '/', '+' };
-            for (int i = 0; i < str.Length; i++)
-            {
-                if (testOperation.Contains(str[i]))
-                {
-                    chars.Add(str[i]);
-                }
-            }
-            Console.WriteLine(string.Join(" ", chars));
-        }
+                numbers.Add(num);
+            }                       
+            return numbers;
+        }                   
     }
 }
